8edd64a15cf7791b0d3c07d3744d04c8a9815353
Jaden Weiss <jadr2ddude@gmail.com>
2017-10-18 22:25:20 -0400

    fix ssl certs

--- builders/bmake.mk
+++ builders/bmake.mk
@@ -1,7 +1,7 @@
 all: image
 
 Dockerfile:
-	cat $(BASES)/$(shell basename $(shell pwd))-$(ARCH) ../Dockerfile.in > Dockerfile
+	cat $(BASES)/$(shell basename $(shell pwd))-$(ARCH) ../Dockerfile.in Dockerfile.in > Dockerfile
 
 scripts:
 	$(MAKE) -C ../scripts dump DESTDIR=$(shell pwd)
